1da905f614e6c5084ec23066f1068e20935563ea
Alex Hernandez <vlexhndz@gmail.com>
2020-08-18 10:19:47 +0200

    faeefewa

--- frontend/src/components/Layout/InfoSlider.js
+++ frontend/src/components/Layout/InfoSlider.js
@@ -86,11 +86,11 @@ const InfoSlider = () => {
                   </div>
                 </div>
                 <div className="row w-100 align-items-center mb-5">
-                  <div className="col-lg-7">
+                  <div className="col-lg-7 order-3">
                     <img src="https://via.placeholder.com/450x252" alt="" />
                   </div>
-                  <div className="d-block d-lg-none m-2"></div>
-                  <div className="col-lg-5">
+                  <div className="d-block d-lg-none m-2 order-2"></div>
+                  <div className="col-lg-5 order-1">
                     <span className="h5 text-dark d-block">
                       Comprometida con los tiempos que corren
                     </span>
@@ -118,11 +118,11 @@ const InfoSlider = () => {
                   </div>
                 </div>
                 <div className="row w-100 align-items-center">
-                  <div className="col-lg-7">
+                  <div className="col-lg-7 order-3">
                     <img src="https://via.placeholder.com/450x252" alt="" />
                   </div>
-                  <div className="d-block d-lg-none m-2"></div>
-                  <div className="col-lg-5">
+                  <div className="d-block d-lg-none m-2 order-2"></div>
+                  <div className="col-lg-5 order-1">
                     <span className="h5 text-dark d-block">
                       I si lo pruebas y no te convence
                     </span>
@@ -136,38 +136,67 @@ const InfoSlider = () => {
               </div>
               <div className="info-slider slick-element text-center p-4 font-weight-normal">
                 <div className="row w-100 align-items-center mb-5">
-                  <div className="d-block d-lg-none m-2"></div>
-                  <div className="col-lg-7">
+                  <div className="col-lg-7 order-3">
                     <img src="https://via.placeholder.com/450x252" alt="" />
                   </div>
-                  <div className="col-lg-5">
-                    <span className="h5 text-dark">
-                      Forma parte de tu academia completamente digital
+                  <div className="d-block d-lg-none m-2 order-2"></div>
+                  <div className="col-lg-5 order-1">
+                    <span className="h5 text-dark d-block">
+                      Forma parte de tu academia de siempre ahora completamente
+                      digital
+                    </span>
+                    <span className="text-grey">
+                      El mundo esta cambiando, no dejes pasar esta oportunidad
                     </span>
                   </div>
                 </div>
                 <div className="row w-100 align-items-center mb-5">
                   <div className="col-lg-5">
-                    <span className="h5 text-dark">
+                    <span className="h5 text-dark d-block">
                       Comprometida con los tiempos que corren
                     </span>
-                  </div>
-                  <div className="col-lg-7">
-                    <img src="https://via.placeholder.com/450x252" alt="" />
+                    <span className="text-grey">
+                      Con nuestras academias no correras riesgo de contagiarte
+                      ni de contagiar a tus seres queridos
+                    </span>
                   </div>
                   <div className="d-block d-lg-none m-2"></div>
-                </div>
-                <div className="row w-100 align-items-center mb-5">
                   <div className="col-lg-7">
                     <img src="https://via.placeholder.com/450x252" alt="" />
                   </div>
-                  <div className="col-lg-5">
-                    <span className="h5 text-dark">
+                </div>
+                <div className="row w-100 align-items-center mb-5">
+                  <div className="col-lg-7 order-3">
+                    <img src="https://via.placeholder.com/450x252" alt="" />
+                  </div>
+                  <div className="d-block d-lg-none m-2 order-2"></div>
+                  <div className="col-lg-5 order-1">
+                    <span className="h5 text-dark d-block">
                       Donde podrás acceder a classes online, videos, listas de
                       reproducción y podcasts
                     </span>
+                    <span className="text-grey">
+                      Te vamos a demostrar que con nuestras academias puedes
+                      seguir aprendiendo y divirtiendote desde cualquier lugar
+                      del mundo
+                    </span>
+                  </div>
+                </div>
+                <div className="row w-100 align-items-center">
+                  <div className="col-lg-5">
+                    <span className="h5 text-dark d-block">
+                      I si lo pruebas y no te convence
+                    </span>
+                    <span className="text-grey">
+                      Tienes garantizada la devolución del último pago de por
+                      vida, siempre que sea la primera vez que te apuntas a esa
+                      academia
+                    </span>
                   </div>
                   <div className="d-block d-lg-none m-2"></div>
+                  <div className="col-lg-7">
+                    <img src="https://via.placeholder.com/450x252" alt="" />
+                  </div>
                 </div>
               </div>
             </Slider>
