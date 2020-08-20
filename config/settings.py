49eb7d59a6b78e18b57331ca3e3306973b8b7c05
Alex Hernandez <vlexhndz@gmail.com>
2020-08-20 18:33:09 +0200

    fefaew

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'STORAGE_BUCKET_NAME' in os.environ:
