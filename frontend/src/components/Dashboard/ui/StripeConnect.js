16a36130d1f4ca91f08c5c40dde6da1aeff2f060
Alex Hernandez <vlexhndz@gmail.com>
2020-08-29 17:32:53 +0200

    efwafawe

--- frontend/src/components/Dashboard/ui/StripeConnect.js
+++ frontend/src/components/Dashboard/ui/StripeConnect.js
@@ -121,7 +121,7 @@ const StripeConnect = (props) => {
               .commercial_stripe_account_id == undefined ? (
               <>
                 <a
-                  href="https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=ca_HmRky5LBHShFfC92Xzjsz0Mj82piwIiy&scope=read_write&redirect_uri=http://192.168.1.10:8000/dashboard/profile"
+                  href="https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=ca_HmRkLTjyLDqt32B5GRlzOhlqeH4ry79e&scope=read_write&redirect_uri=http://192.168.1.10:8000/dashboard/profile"
                   className="connect-button"
                 >
                   {/* prod */}
