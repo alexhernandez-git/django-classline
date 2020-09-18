dff424ad76e0ecf42144e98054748da5454db0a8
Alex Hernandez <vlexhndz@gmail.com>
2020-09-18 20:49:48 +0200

    efawfwae

--- api/users/serializers/users.py
+++ api/users/serializers/users.py
@@ -99,7 +99,7 @@ def send_request_demo(data):
     subject = 'Soy {} y quiero una demo para mi empresa que se llama {}!'.format(
         data['first_name'], data['company_name'])
     from_email = 'Classline Academy <no-reply@classlineacademy.com>'
-    to_email = 'vlexhndz@gmail.com'
+    to_email = 'ah30456@gmail.com'
     content = render_to_string(
         'emails/users/request_demo.html',
         {
