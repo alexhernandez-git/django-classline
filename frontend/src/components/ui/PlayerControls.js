70dbc25a48501f79b2f97959db2402be9ec84af3
Alex Hernandez <vlexhndz@gmail.com>
2020-11-30 12:41:12 +0100

    ewfewa

--- frontend/src/components/ui/PlayerControls.js
+++ frontend/src/components/ui/PlayerControls.js
@@ -209,7 +209,11 @@ const ControlsWrapper = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.6);
+  background: linear-gradient(
+    rgba(0, 0, 0, 0.6),
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.6)
+  );
   display: flex;
   flex-direction: column;
   justify-content: space-between;
