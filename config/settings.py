4e8b33f3700f599b634339232a52a78a222c729f
Alex <vlexhndz@gmail.com>
2021-03-04 15:57:37 +0100

    fewfew

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -363,3 +363,4 @@ CELERY_BROKER_TRANSPORT_OPTIONS = {
 }
 
 FILE_UPLOAD_HANDLERS = ['django.core.files.uploadhandler.TemporaryFileUploadHandler']
+django_heroku.settings(locals())
